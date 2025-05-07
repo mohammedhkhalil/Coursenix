6b65e4c9525269b7465f2f1c9403923685c7da2e
Hisham-sayed <hishamsayed995@gmail.com>
2025-05-07 20:12:54 +0300

    Forget & creat password

---
+++ Coursenix/Controllers/PasswordController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Coursenix.Controllers
+{
+    public class PasswordController : Controller
+    {
+        public IActionResult CreatePass()
+        {
+            return View();
+        }
+
+        public IActionResult ForgetPass()
+        {
+            return View();
+        }
+    }
+}
