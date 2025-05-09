7df9b7da1041dc8d13d449bf27000fc3c482c866
Abdokamal1532 <abdokamal562004@gmail.com>
2025-05-09 16:59:49 +0300

    +

--- Coursenix/Models/Admin.cs
+++ Coursenix/Models/Admin.cs
@@ -1,28 +1,12 @@
-﻿// Models/Admin.cs
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Coursenix.Models
 {
     [Table("Admins")]
     public class Admin
     {
-        [Key]
-        public int AdminId { get; set; }
 
-        [Required]
-        [MaxLength(255)]
-        public string Email { get; set; }
-
-        [Required]
-        [MaxLength(255)]
-        public string Password { get; set; } // تذكر استخدام التشفير لكلمات المرور في التطبيق الفعلي
-
-        [MaxLength(50)]
-        public string FirstName { get; set; }
-
-        [MaxLength(50)]
-        public string LastName { get; set; }
+        // Any admin-specific properties could be added here
 
     }
 }
