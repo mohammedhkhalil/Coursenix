7e1a3753b7800ef07b4cb5017e409a1f85ecb48d
Abdokamal1532 <abdokamal562004@gmail.com>
2025-05-09 17:05:10 +0300

    +

--- Coursenix/Models/CoursenixContext.cs
+++ Coursenix/Models/CoursenixContext.cs
@@ -1,18 +1,10 @@
-﻿// Models/CoursenixContext.cs
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 
 namespace Coursenix.Models
 {
     public class CoursenixContext : DbContext
     {
-        public CoursenixContext(DbContextOptions<CoursenixContext> options)
-            : base(options)
-        {
-        }
 
         // DbSets for all entities
         public DbSet<Student> Students { get; set; }
@@ -24,30 +16,10 @@ namespace Coursenix.Models
         public DbSet<Booking> Bookings { get; set; }
         public DbSet<Attendance> Attendances { get; set; }
 
-
-        // أضف أو قم بتعديل هذه الدالة
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            base.OnModelCreating(modelBuilder); // من المهم استدعاء الدالة الأساسية أولاً
-
-            // تحديد الدقة والمقياس للخاصية Price في الكيان Subject
-            modelBuilder.Entity<Subject>()
-                .Property(s => s.Price)
-                .HasPrecision(18, 2);
-
-            // مثال: تحديد العلاقات بين الكيانات بشكل صريح إذا لم يتم اكتشافها تلقائياً أو لتخصيصها
-            modelBuilder.Entity<Subject>()
-               .HasOne(s => s.Teacher)
-               .WithMany(t => t.Subjects)
-               .HasForeignKey(s => s.TeacherId);
-
-            modelBuilder.Entity<Group>()
-               .HasOne(g => g.Subject)
-               .WithMany(s => s.Groups)
-               .HasForeignKey(g => g.SubjectId);
-
-            // ... إلخ لعلاقات Booking, Session, Attendance
-
+            //optionsBuilder.UseSqlServer(@"Data Source=HISHAMSAYED;Initial Catalog=Coursenix1;Integrated Security=True");
+            //base.OnConfiguring(optionsBuilder);
         }
     }
 }
