feef864109f8e29225b7f297d2a9d26c5283bdc7
Wafaa0Sayed <wafaa304010@gmail.com>
2025-05-29 02:30:33 +0300

    studentAttendance فاضل جزء ال js

--- Coursenix/Controllers/GroupsController.cs
+++ Coursenix/Controllers/GroupsController.cs
@@ -24,53 +24,54 @@ namespace Coursenix.Controllers
         {
             _context = context;
         }
-        //public async Task<IActionResult> studentAttendance(int groupId)
-        //{
-        //    var group = await _context.Groups
-        //        .Include(g => g.Bookings)
-        //            .ThenInclude(b => b.Student)
-        //        .Include(g => g.Sessions)
-        //            .ThenInclude(s => s.Attendances)
-        //        .FirstOrDefaultAsync(g => g.Id == groupId);
+        public async Task<IActionResult> studentAttendance(int groupId)
+        {
+            var group = await _context.Groups
+                .Include(g => g.Bookings)
+                    .ThenInclude(b => b.Student)
+                .Include(g => g.Sessions)
+                    .ThenInclude(s => s.Attendances)
+                .FirstOrDefaultAsync(g => g.Id == groupId);
 
 
 
-        //    if (group == null)
-        //        return NotFound();
+            if (group == null)
+                return NotFound();
 
-        //    var totalSessions = group.Sessions.Count;
-        //    // list
-        //    var studentAttendances = group.Bookings
-        //        .Select(b =>
-        //        {
-        //            var student = b.Student;
-        //            var attendedCount = group.Sessions
-        //                .Count(s => s.Attendances.Any(a => a.Student.Id == student.Id));
+            var totalSessions = group.Sessions.Count;
+            // list
+            var studentAttendances = group.Bookings
+                .Select(b =>
+                {
+                    // for each el
+                    var student = b.Student;
+                    var attendedCount = group.Sessions // like we iterate over all session list and count how many session that sutdent attentd
+                        .Count(s => s.Attendances.Any(a => a.Student.Id == student.Id));
 
-        //            var absencePercentage = totalSessions == 0 ? 0 : (1 - (attendedCount / (double)totalSessions)) * 100;
+                    var absencePercentage = totalSessions == 0 ? 0 : (1 - (attendedCount / (double)totalSessions)) * 100;
 
-        //            return new GroupAttendanceViewModel.StudentAttendance
-        //            {
-        //                Id = student.Id,
-        //                Name = student.Name,
-        //                PhoneNumber = student.PhoneNumber,
-        //                ParentPhoneNumber = student.ParentPhoneNumber,
-        //                AbsencePercentage = Math.Round(absencePercentage, 1)
-        //            };
-        //        }).ToList();
+                    return new GroupAttendanceViewModel.StudentAttendance
+                    {
+                        Id = student.Id,
+                        Name = student.Name,
+                        PhoneNumber = student.PhoneNumber,
+                        ParentPhoneNumber = student.ParentPhoneNumber,
+                        AbsencePercentage = Math.Round(absencePercentage, 1)
+                    };
+                }).ToList();
 
-        //    var viewModel = new GroupAttendanceViewModel
-        //    {
-        //        GradeLevel = group.Subject.GradeLevel,
-        //        GroupName = group.Name,
-        //        //Days = group.Days,
-        //        StartTime = group.StartTime,
-        //        EndTime = group.EndTime,
-        //        Students = studentAttendances
-        //    };
+            var viewModel = new GroupAttendanceViewModel
+            {
+                GradeLevel = group.GradeLevel.NumberOfGrade,
+                GroupName = group.Name,
+                Days = group.SelectedDays,
+                StartTime = group.StartTime,
+                EndTime = group.EndTime,
+                Students = studentAttendances
+            };
 
-        //    return View(viewModel);
-        //}
+            return View(viewModel);
+        }
 
         ////SessionAttendance
         //// GET: Attendance/TakeAttendance?sessionId=5
--- Coursenix/Controllers/StudentController.cs
+++ Coursenix/Controllers/StudentController.cs
@@ -1,165 +1,174 @@
-﻿//using Coursenix.Models;
-//using Coursenix.Models.ViewModels;
-//using Coursenix.ViewModels;
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
+﻿using Coursenix.Models;
+using Coursenix.Models.ViewModels;
+using Coursenix.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-//namespace Coursenix.Controllers
-//{
-//    public class StudentController : Controller
-//    {
-//        //private readonly Context _context = new Context()
+namespace Coursenix.Controllers
+{
+    public class StudentController : Controller
+    {
+        //private readonly Context _context = new Context()
 
-//        // all view of student 
-//        private readonly Context _context;
-//        private readonly UserManager<AppUser> _userManager;
-//        private readonly SignInManager<AppUser> _signInManager;
-//        public StudentController(UserManager<AppUser> userManager, Context context, SignInManager<AppUser> signInManager)
-//        {
-//            _userManager = userManager;
-//            _context = context;
-//            _signInManager = signInManager;
-//        }
+        // all view of student 
+        private readonly Context _context;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        public StudentController(UserManager<AppUser> userManager, Context context, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _context = context;
+            _signInManager = signInManager;
+        }
 
-//        [HttpGet]
-//        public async Task<IActionResult> Settings()
-//        {
-//            var userId = _userManager.GetUserId(User);
+        [HttpGet]
+        public async Task<IActionResult> Settings()
+        {
+            var userId = _userManager.GetUserId(User);
 
-//            var student = await _context.Students
-//                .Include(s => s.AppUser)
-//                .FirstOrDefaultAsync(s => s.AppUserId == userId);
+            var student = await _context.Students
+                .Include(s => s.AppUser)
+                .FirstOrDefaultAsync(s => s.AppUserId == userId);
 
-//            if (student == null) return NotFound();
+            if (student == null) return NotFound();
 
-//            var vm = new StudentSettingsVM
-//            {
-//                Name = student.Name,
-//                PhoneNumber = student.PhoneNumber,
-//                ParentPhoneNumber = student.ParentPhoneNumber,
-//                Grade = student.Grade
-//            };
+            var vm = new StudentSettingsVM
+            {
+                Name = student.Name,
+                PhoneNumber = student.PhoneNumber,
+                ParentPhoneNumber = student.ParentPhoneNumber,
+                Grade = student.Grade
+            };
 
-//            return View(vm);
-//        }
+            return View(vm);
+        }
 
-//        [HttpPost]
-//        public async Task<IActionResult> Settings(StudentSettingsVM model)
-//        {
-//            var userId = _userManager.GetUserId(User);
+        [HttpPost]
+        public async Task<IActionResult> Settings(StudentSettingsVM model)
+        {
+            var userId = _userManager.GetUserId(User);
 
-//            var student = await _context.Students
-//                .Include(s => s.AppUser)
-//                .FirstOrDefaultAsync(s => s.AppUserId == userId);
+            var student = await _context.Students
+                .Include(s => s.AppUser)
+                .FirstOrDefaultAsync(s => s.AppUserId == userId);
 
-//            if (student == null) return NotFound();
+            if (student == null) return NotFound();
 
-//            if (!string.IsNullOrWhiteSpace(model.Name))
-//                student.Name = model.Name;
+            if (!string.IsNullOrWhiteSpace(model.Name))
+                student.Name = model.Name;
 
-//            if (!string.IsNullOrWhiteSpace(model.PhoneNumber))
-//                student.PhoneNumber = model.PhoneNumber;
+            if (!string.IsNullOrWhiteSpace(model.PhoneNumber))
+                student.PhoneNumber = model.PhoneNumber;
 
-//            if (!string.IsNullOrWhiteSpace(model.ParentPhoneNumber))
-//                student.ParentPhoneNumber = model.ParentPhoneNumber;
+            if (!string.IsNullOrWhiteSpace(model.ParentPhoneNumber))
+                student.ParentPhoneNumber = model.ParentPhoneNumber;
 
-//            //if (model.Grade.HasValue)
-//            //    student.Grade = model.Grade.Value;
+            //if (model.Grade.HasValue)
+            //    student.Grade = model.Grade.Value;
 
-//            await _userManager.UpdateAsync(student.AppUser);
-//            _context.Update(student);
-//            await _context.SaveChangesAsync();
+            await _userManager.UpdateAsync(student.AppUser);
+            _context.Update(student);
+            await _context.SaveChangesAsync();
 
-//            // Password change 
-//            if (!string.IsNullOrEmpty(model.CurrPassword) && !string.IsNullOrEmpty(model.NewPassword))
-//            {
-//                var result = await _userManager.ChangePasswordAsync(student.AppUser, model.CurrPassword, model.NewPassword);
-//                if (!result.Succeeded)
-//                {
-//                    foreach (var error in result.Errors)
-//                        ModelState.AddModelError(string.Empty, error.Description);
-//                    return View(model);
-//                }
+            // Password change 
+            if (!string.IsNullOrEmpty(model.CurrPassword) && !string.IsNullOrEmpty(model.NewPassword))
+            {
+                var result = await _userManager.ChangePasswordAsync(student.AppUser, model.CurrPassword, model.NewPassword);
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    return View(model);
+                }
 
-//                // sgin in 
-//                await _signInManager.RefreshSignInAsync(student.AppUser);
-//            }
+                // sgin in 
+                await _signInManager.RefreshSignInAsync(student.AppUser);
+            }
 
-//            TempData["SuccessMessage"] = "Settings updated successfully.";
-//            return RedirectToAction("Index", "Courses");
-//        }
+            TempData["SuccessMessage"] = "Settings updated successfully.";
+            return RedirectToAction("Index", "Courses");
+        }
 
-//        public async Task<IActionResult> Dashboard()
-//        {
-//            var appUser = await _userManager.GetUserAsync(User);
-//            if (appUser is null)
-//                return Challenge();   // force login
+        public async void DeleteStudent(int StudentId)
+        {
+            var student = await _context.Students.FindAsync(StudentId);
 
-//            var student = await _context.Students
-//                .FirstOrDefaultAsync(s => s.AppUserId == appUser.Id);
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
 
-//            if (student is null)
-//                return NotFound("Student profile not found.");
+        }
 
-//            // All bookings for this student (=> groups)
-//            var bookings = await _context.Bookings
-//                .Where(b => b.StudentId == student.Id)
-//                .Include(b => b.Group)
-//                    .ThenInclude(g => g.Subject)
-//                        .ThenInclude(su => su.Teacher)
-//                .Include(b => b.Group.GroupDays)
-//                .ToListAsync();
+        //public async Task<IActionResult> Dashboard()
+        //{
+        //    var appUser = await _userManager.GetUserAsync(User);
+        //    if (appUser is null)
+        //        return Challenge();   // force login
 
-//            // Build the view-model list
-//            var viewModel = new List<StudentGroupViewModel>();
+        //    var student = await _context.Students
+        //        .FirstOrDefaultAsync(s => s.AppUserId == appUser.Id);
 
-//            foreach (var booking in bookings)
-//            {
-//                var group = booking.Group;
+        //    if (student is null)
+        //        return NotFound("Student profile not found.");
 
-//                // a) All sessions in this group
-//                var sessionIds = await _context.Sessions
-//                    .Where(se => se.GroupId == group.Id)
-//                    .Select(se => se.Id)
-//                    .ToListAsync();
+        //    // All bookings for this student (=> groups)
+        //    var bookings = await _context.Bookings
+        //        .Where(b => b.StudentId == student.Id)
+        //        .Include(b => b.Group)
+        //            .ThenInclude(g => g.Subject)
+        //                .ThenInclude(su => su.Teacher)
+        //        .Include(b => b.Group.GroupDays)
+        //        .ToListAsync();
 
-//                var totalSessions = sessionIds.Count;
+        //    // Build the view-model list
+        //    var viewModel = new List<StudentGroupViewModel>();
 
-//                // b) How many the student attended
-//                var sessionsAttended = await _context.Attendances
-//                    .CountAsync(a =>
-//                        a.StudentId == student.Id &&
-//                        a.IsPresent &&
-//                        sessionIds.Contains(a.SessionId));
+        //    foreach (var booking in bookings)
+        //    {
+        //        var group = booking.Group;
 
-//                // c) One dashboard card
-//                viewModel.Add(new StudentGroupViewModel
-//                {
-//                    StudentId = student.Id,
-//                    StudentName = student.Name,
+        //        // a) All sessions in this group
+        //        var sessionIds = await _context.Sessions
+        //            .Where(se => se.GroupId == group.Id)
+        //            .Select(se => se.Id)
+        //            .ToListAsync();
 
-//                    GroupId = group.Id,
-//                    GroupName = group.Name,
-//                    SubjectName = group.Subject.SubjectName,
-//                    TeacherName = group.Subject.Teacher.Name,
+        //        var totalSessions = sessionIds.Count;
 
-//                    Days = group.GroupDays
-//                                        .Select(d => d.Day.ToString())
-//                                        .ToList(),
-//                    StartTime = group.StartTime,
-//                    EndTime = group.EndTime,
-//                    Location = group.Location,
+        //        // b) How many the student attended
+        //        var sessionsAttended = await _context.Attendances
+        //            .CountAsync(a =>
+        //                a.StudentId == student.Id &&
+        //                a.IsPresent &&
+        //                sessionIds.Contains(a.SessionId));
 
-//                    TotalSessions = totalSessions,
-//                    SessionsAttended = sessionsAttended
-//                });
-//            }
+        //        // c) One dashboard card
+        //        viewModel.Add(new StudentGroupViewModel
+        //        {
+        //            StudentId = student.Id,
+        //            StudentName = student.Name,
 
-//            return View("Dashboard", viewModel);
-//        }
+        //            GroupId = group.Id,
+        //            GroupName = group.Name,
+        //            SubjectName = group.Subject.SubjectName,
+        //            TeacherName = group.Subject.Teacher.Name,
 
-//    }
+        //            Days = group.GroupDays
+        //                                .Select(d => d.Day.ToString())
+        //                                .ToList(),
+        //            StartTime = group.StartTime,
+        //            EndTime = group.EndTime,
+        //            Location = group.Location,
+
+        //            TotalSessions = totalSessions,
+        //            SessionsAttended = sessionsAttended
+        //        });
+        //    }
+
+        //    return View("Dashboard", viewModel);
+        //}
+
+    }
 
 
-//}
+}
