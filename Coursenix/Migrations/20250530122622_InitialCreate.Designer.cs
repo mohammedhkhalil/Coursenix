4aa4afd61d4893c6d30aad5de918334d580203e2
marymb2087@gmail.com <marymb2087@gmail.com>
2025-05-30 15:40:59 +0300

    ......

--- Coursenix/Migrations/20250530122622_InitialCreate.Designer.cs
+++ Coursenix/Migrations/20250530122622_InitialCreate.Designer.cs
@@ -4,6 +4,7 @@ using Coursenix.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Coursenix.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20250530122622_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
