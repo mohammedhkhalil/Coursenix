5bc9e63eb6b3f4720b4ec861ef6a5e0d681dd3fe
Hisham-sayed <hishamsayed995@gmail.com>
2025-05-31 03:11:21 +0300

    d7k

--- Coursenix/ViewModels/StudentDashboardViewModel.cs
+++ Coursenix/ViewModels/StudentDashboardViewModel.cs
@@ -1,23 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Coursenix.Models
+namespace Coursenix.ViewModels
 {
     public class StudentDashboardViewModel
     {
+        public string StudentName { get; set; } // For user profile display
+        public List<CourseInfo> Courses { get; set; } = new List<CourseInfo>();
+
         public class CourseInfo
         {
             public string CourseName { get; set; }
             public string TeacherName { get; set; }
             public string GroupName { get; set; }
             public string Location { get; set; }
-            public string Days { get; set; } 
-            public string Time { get; set; } 
-            public double AbsenceRatio { get; set; } // Percentage
-            public string AbsenceClass { get; set; } // "low", "medium", "high"
+            public List<string> Days { get; set; }
+            public string TimeRange { get; set; }
+            public string AbsenceRatio { get; set; }
+            public string AbsenceClass { get; set; } // For CSS: "low", "medium", etc.
         }
-
-        public string StudentName { get; set; }
-        public List<CourseInfo> Courses { get; set; } = new List<CourseInfo>();
     }
 }
